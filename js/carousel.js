d271b130d6212aea907919ff8019e44fb6825564
DionigiMartino <dionigi49@gmail.com>
2020-08-12 17:52:47 +0200

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,9 @@
+// Creating the Class For the Carousel
+
+class Carousel {
+    constructor(container, title, subtitle){
+        this.container = container;
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+}
